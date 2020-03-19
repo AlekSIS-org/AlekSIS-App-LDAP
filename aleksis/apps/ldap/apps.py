b55d2b86ca7a654f959d533c30ae2a0471c653f3
Tom Teichler <tom.teichler@teckids.org>
2020-03-20 00:16:15 +0100

    Register ready method

--- aleksis/apps/ldap/apps.py
+++ aleksis/apps/ldap/apps.py
@@ -1,6 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+
 from aleksis.core.util.apps import AppConfig
 
+from .util.ldap_sync import ldap_create_user
 
-class DefaultConfig(AppConfig):
+class LDAPConfig(AppConfig):
     name = "aleksis.apps.ldap"
     verbose_name = "AlekSIS — LDAP (General LDAP import/export)"
+
+    def ready() -> None:
+        super().ready()
+        User = get_user_model()
+        post_save.connect(ldap_create_user, sender=User)
