03bc1d061981d35d632142ada2d5b74b4fc86b4e
Tom Teichler <tom.teichler@teckids.org>
2020-03-21 22:16:58 +0100

    Add code comments and docstring

--- aleksis/apps/ldap/util/ldap_sync.py
+++ aleksis/apps/ldap/util/ldap_sync.py
@@ -3,12 +3,16 @@ from django.contrib.auth import get_user_model
 
 from constance import config
 
+
 def ldap_create_user(sender, instance, created, raw, using, update_fields, **kwargs):
+    """ Find ldap users by configurable matching fields and connect them to django users. """
     Person = apps.get_model("core", "Person")
 
     if config.ENABLE_LDAP_SYNC and (created or config.LDAP_SYNC_ON_UPDATE):
+        # Check if there is an existing person connected to the user.
         if not Person.objects.filter(user=instance).exists():
             if config.LDAP_MATCHING_FIELDS == 'match-email':
+                # Get or create a person matching to email field.
                 person, created = Person.objects.get_or_create(
                     email=instance.email,
                     defaults={
@@ -17,6 +21,7 @@ def ldap_create_user(sender, instance, created, raw, using, update_fields, **kwa
                     }
                 )
             elif config.LDAP_MATCHING_FIELDS == 'match-name':
+                # Get or create a person matching to the first and last name.
                 person, created = Person.objects.get_or_create(
                     first_name=instance.first_name,
                     last_name=instance.last_name,
@@ -25,12 +30,14 @@ def ldap_create_user(sender, instance, created, raw, using, update_fields, **kwa
                     }
                 )
             elif config.LDAP_MATCHING_FIELDS == 'match-email-name':
+                # Get or create a person matching to the email and the first and last name.
                 person, created = Person.objects.get_or_create(
                     first_name=instance.first_name,
                     last_name=instance.last_name,
                     email=instance.email
                 )
 
+            # Save person if enabled in config or no new person was created.
             if config.LDAP_SYNC_CREATE or not created:
                 person.user = instance
                 person.save()
