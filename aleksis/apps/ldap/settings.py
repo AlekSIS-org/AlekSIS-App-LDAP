77bcf50f7a36a672e5c0ab8044cc564782aa8166
Tom Teichler <tom.teichler@teckids.org>
2020-03-21 21:25:53 +0100

    Add way to configure matching fields

--- aleksis/apps/ldap/settings.py
+++ aleksis/apps/ldap/settings.py
@@ -8,12 +8,21 @@ CONSTANCE_ADDITIONAL_FIELDS = {
                     ("match-create", _("Match and create persons")),
                     )
     }],
+    "matching-fields-select": ['django.forms.fields.ChoiceField', {
+        'widget': 'django.forms.Select',
+        'choices': ((None, "-----"),
+                    ("match-email", _("Match only on email")),
+                    ("match-name", _("Match only on name")),
+                    ("match-email-name", _("Match on email and name")),
+                    )
+    }],
 }
 
 CONSTANCE_CONFIG = {
-    "ENABLE_LDAP_SYNC": ("yes", _("Enable ldap sync"), "bool"),
-    "LDAP_SYNC_STRATEGY": ("None", _("Strategy for ldap sync"), "strategy-select"),
+    "ENABLE_LDAP_SYNC": (True, _("Enable ldap sync"), bool),
+    "LDAP_SYNC_STRATEGY": (None, _("Strategy for ldap sync"), "strategy-select"),
+    "LDAP_MATCHING_FIELDS": (None, _("LDAP sync matching fields"), "matching-fields-select"),
 }
 CONSTANCE_CONFIG_FIELDSETS = {
-    "LDAP-Sync settings": ("ENABLE_LDAP_SYNC", "LDAP_SYNC_STRATEGY"),
+    "LDAP-Sync settings": ("ENABLE_LDAP_SYNC", "LDAP_SYNC_STRATEGY", "LDAP_MATCHING_FIELDS"),
 }
--- aleksis/apps/ldap/util/ldap_sync.py
+++ aleksis/apps/ldap/util/ldap_sync.py
@@ -8,24 +8,54 @@ def ldap_create_user(sender, **kwargs):
     if config.ENABLE_LDAP_SYNC:
         if not sender.person:
             if config.LDAP_SYNC_STRATEGY == 'match-create':
-                if not Person.objects.get(full_name=sender.get_full_name()) or not Person.objects.get(email=sender.email):
-                    person = Person.objects.create(
-                        first_name = sender.first_name,
-                        last_name = sender.last_name,
-                        email = sender.email,
-                        sender = sender,
-                    )
-                    sender.person = person
-                    sender.save()
-                else:
-                    if Person.objects.get(full_name=sender.get_full_name()):
-                        person = Person.objects.get(full_name=sender.get_full_name())
+                if config.LDAP_MATCHING_FIELDS == 'match-email':
+                    if not Person.objects.get(email=sender.email):
+                        person = Person.objects.create(
+                            first_name = sender.first_name,
+                            last_name = sender.last_name,
+                            email = sender.email,
+                            sender = sender,
+                        )
                         sender.person = person
                         sender.save()
-                    elif Person.objects.get(email=sender.email):
+                    else:
+                        Person.objects.get(email=sender.email):
                         person = Person.objects.get(email=sender.email)
                         sender.person = person
                         sender.save()
+                if config.LDAP_MATCHING_FIELDS == 'match-name':
+                    if not Person.objects.get(first_name=sender.first_name, last_name=sender.last_name):
+                        person = Person.objects.create(
+                            first_name = sender.first_name,
+                            last_name = sender.last_name,
+                            email = sender.email,
+                            sender = sender,
+                        )
+                        sender.person = person
+                        sender.save()
+                    else:
+                        person = Person.objects.get(first_name=sender.first_name, last_name=sender.last_name)
+                        sender.person = person
+                        sender.save()
+                if config.LDAP_MATCHING_FIELDS == 'match-email-name':
+                    if not Person.objects.get(first_name=sender.first_name, last_name=sender.last_name) and not Person.objects.get(email=sender.email):
+                        person = Person.objects.create(
+                            first_name = sender.first_name,
+                            last_name = sender.last_name,
+                            email = sender.email,
+                            sender = sender,
+                        )
+                        sender.person = person
+                        sender.save()
+                    else:
+                        if Person.objects.get(first_name=sender.first_name, last_name=sender.last_name):
+                            person = Person.objects.get(first_name=sender.first_name, last_name=sender.last_name)
+                            sender.person = person
+                            sender.save()
+                        elif Person.objects.get(email=sender.email):
+                            person = Person.objects.get(email=sender.email)
+                            sender.person = person
+                            sender.save()
             elif config.LDAP_SYNC_STRATEGY == 'match-only':
                 if Person.objects.get(full_name=sender.get_full_name()):
                     person = Person.objects.get(full_name=sender.get_full_name())
