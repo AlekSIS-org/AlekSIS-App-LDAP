b3da10d24f6f6f51d19b2932c231e238c5339542
Tom Teichler <tom.teichler@teckids.org>
2020-03-27 13:28:12 +0100

    Add missing settings

--- aleksis/apps/ldap/settings.py
+++ aleksis/apps/ldap/settings.py
@@ -17,13 +17,15 @@ CONSTANCE_ADDITIONAL_FIELDS = {
 
 CONSTANCE_CONFIG = {
     "ENABLE_LDAP_SYNC": (True, _("Enable ldap sync"), bool),
-    "LDAP_SYNC_CREATE": (True, _("Match created persons to users"), bool),
+    "LDAP_SYNC_CREATE": (True, _("Create non-existing persons"), bool),
     "LDAP_SYNC_ON_UPDATE": (True, _("Also sync if user updates"), bool),
     "LDAP_MATCHING_FIELDS": (
         None,
         _("LDAP sync matching fields"),
         "matching-fields-select",
     ),
+    "ENABLE_LDAP_GROUP_SYNC": (True, _("Enable ldap group sync"), bool),
+    "LDAP_SYNC_CREATE_GROUPS": (True, _("Create non-existing groups"), bool),
     "LDAP_GROUP_SYNC_FIELD_SHORT_NAME": ("cn", _("Field for short name of group"), str),
     "LDAP_GROUP_SYNC_FIELD_NAME": ("cn", _("Field for name of group"), str),
 }
@@ -33,6 +35,8 @@ CONSTANCE_CONFIG_FIELDSETS = {
         "LDAP_SYNC_CREATE",
         "LDAP_SYNC_ON_UPDATE",
         "LDAP_MATCHING_FIELDS",
+        "ENABLE_LDAP_GROUP_SYNC",
+        "LDAP_SYNC_CREATE_GROUPS",
         "LDAP_GROUP_SYNC_FIELD_SHORT_NAME",
         "LDAP_GROUP_SYNC_FIELD_NAME",
     ),
