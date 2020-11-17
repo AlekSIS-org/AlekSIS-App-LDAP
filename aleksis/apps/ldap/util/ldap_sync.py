ac4262f7a959cec61af1fe4e2e9aaa8e045eed04
Dominik George <nik@naturalnet.de>
2020-11-17 21:30:55 +0100

    Replace double underscores with single
    This cna lead to name clashes, but we tolerate these in favour of apps
    being allowed to properly namespace their proxy fields.

--- aleksis/apps/ldap/util/ldap_sync.py
+++ aleksis/apps/ldap/util/ldap_sync.py
@@ -29,8 +29,8 @@ TQDM_DEFAULTS = {
 
 def setting_name_from_field(model, field):
     """Generate a setting name from a model field."""
-    part_1 = model._meta.label_lower.replace(".", "_")
-    return f"additional_field_{part_1}_{field.name}"
+    part_1 = model._meta.label_lower.replace(".", "_").replace("__", "_")
+    return f"additional_field_{part_1}_{field.name}".replace("__", "_")
 
 
 def ldap_field_to_filename(dn, fieldname):
