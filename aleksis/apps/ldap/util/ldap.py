37036b2fa4731e448fc6a1d4a1406181d04d0ef6
Dominik George <nik@naturalnet.de>
2021-02-09 23:50:42 +0100

    Emit logging message when elevating LDAP privileges

--- aleksis/apps/ldap/util/ldap.py
+++ aleksis/apps/ldap/util/ldap.py
@@ -1,5 +1,9 @@
 """Extension utilities for django-auth-ldap"""
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class TemporaryBind:
     """LDAP conection from an LDAPUser object temporarily bound with other credentials"""
@@ -12,10 +16,12 @@ class TemporaryBind:
     def __enter__(self):
         if self.dn is not None:
             # Bind with defined credentials and mark connection bound
+            logger.info("Temporarily binding to LDAP as %s", self.dn)
             self.ldap_user._bind_as(self.dn, self.password, sticky=True)
         return self.ldap_user.connection
 
     def __exit__(self, type, value, traceback):
         if self.dn is not None:
             # Re-bind with regular credentials so we do not leak connections with elevated privileges
+            logger.info("Ending temporary LDAP bind as %s", self.dn)
             self.ldap_user._bind()
