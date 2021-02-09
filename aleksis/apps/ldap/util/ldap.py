09fad1f4a9b3a87060ac44d6713553a109806af8
Dominik George <nik@naturalnet.de>
2021-02-09 23:52:01 +0100

    Fix variable name dn → bind_dn

--- aleksis/apps/ldap/util/ldap.py
+++ aleksis/apps/ldap/util/ldap.py
@@ -8,20 +8,20 @@ logger = logging.getLogger(__name__)
 class TemporaryBind:
     """LDAP conection from an LDAPUser object temporarily bound with other credentials"""
 
-    def __init__(self, ldap_user, dn, password):
+    def __init__(self, ldap_user, bind_dn, password):
         self.ldap_user = ldap_user
-        self.dn = dn
+        self.bind_dn = bind_dn
         self.password = password
 
     def __enter__(self):
-        if self.dn is not None:
+        if self.bind_dn is not None:
             # Bind with defined credentials and mark connection bound
-            logger.info("Temporarily binding to LDAP as %s", self.dn)
-            self.ldap_user._bind_as(self.dn, self.password, sticky=True)
+            logger.info("Temporarily binding to LDAP as %s", self.bind_dn)
+            self.ldap_user._bind_as(self.bind_dn, self.password, sticky=True)
         return self.ldap_user.connection
 
     def __exit__(self, type, value, traceback):
-        if self.dn is not None:
+        if self.bind_dn is not None:
             # Re-bind with regular credentials so we do not leak connections with elevated privileges
-            logger.info("Ending temporary LDAP bind as %s", self.dn)
+            logger.info("Ending temporary LDAP bind as %s", self.bind_dn)
             self.ldap_user._bind()
